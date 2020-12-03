8391cec613b5e1269f69c2d8c088f0ec6260d289
Timmy <mr.oshnokov@gmail.com>
2020-12-04 00:24:23 +0300

    JavaScript Lab6

---
+++ JavaScriptTesting/Lab6.js
@@ -0,0 +1,35 @@
+function showContacts() {
+    document.getElementById("popUp").style.visibility="visible";
+}
+
+function closeContacts(){
+    document.getElementById("popUp").style.visibility="hidden";
+}
+
+let imageIndex = 1;
+showSlides(imageIndex);
+
+function plusSlides(n) {
+    showSlides(imageIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(imageIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    const images = document.getElementsByClassName("selfImage");
+    console.log(images)
+    const dots = document.getElementsByClassName("dot");
+    if (n > images.length) {imageIndex = 1}
+    if (n < 1) {imageIndex = images.length}
+    for (i = 0; i < images.length; i++) {
+        images[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("active", "");
+    }
+    images[imageIndex-1].style.display = "block";
+    dots[imageIndex-1].className += " active";
+}
